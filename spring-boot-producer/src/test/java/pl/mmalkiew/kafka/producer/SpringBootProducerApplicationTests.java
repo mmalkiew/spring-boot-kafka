c4069b9fa2a49f76e5412182bdefff7fb7fea83f
Michał Małkiewicz <michal.malkiewicz@uniteam.pl>
2019-02-22 13:19:52 +0100

    init spring kafka producer

---
+++ spring-boot-producer/src/test/java/pl/mmalkiew/kafka/producer/SpringBootProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mmalkiew.kafka.producer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) @SpringBootTest public class SpringBootProducerApplicationTests {
+
+    @Test public void contextLoads() {
+    }
+
+}
