1080d24b09339d4cad0442486006357510207161
Michał Małkiewicz <michal.malkiewicz@uniteam.pl>
2019-02-22 13:34:01 +0100

    spring boot consumer init

---
+++ spring-boot-consumer/src/test/java/pl/mmalkiew/kafka/consumer/SpringBootConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mmalkiew.kafka.consumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) @SpringBootTest public class SpringBootConsumerApplicationTests {
+
+    @Test public void contextLoads() {
+    }
+
+}
