52bf2fe3dfdaa16d225ccb3a992a397e6537563e
mmalkiew <michalmalkiewicz90@gmail.com>
2019-05-29 21:42:02 +0200

    kafka consumer example

---
+++ javaproducer/src/main/java/pl/mmalkiew/kafka/consumer/ConsumerDemo.java
@@ -0,0 +1,52 @@
+package pl.mmalkiew.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.mmalkiew.kafka.producer.ProducerDemoWithCallback;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class ConsumerDemo {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+
+    public static void main(String[] args) {
+
+        String bootstrapServers = "127.0.0.1:9092";
+        String groupId = "my_fourth_app";
+        String topic = "first_topic";
+
+        //  create consumer properties
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        //  create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        //  subscribe consumer to our topics
+        consumer.subscribe(Collections.singleton(topic));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record : records) {
+                LOGGER.info("Key {}", record.key());
+                LOGGER.info("Value {}", record.value());
+                LOGGER.info("Partition {}", record.partition());
+                LOGGER.info("Offset {}", record.offset());
+            }
+        }
+
+    }
+}
